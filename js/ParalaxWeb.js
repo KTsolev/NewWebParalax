20f0a0eb0a4da6ef6c00abd76dca7bd351c004ef
KTsolev <ktsolev@gmail.com>
2016-10-18 22:51:46 +0200

    Initial Paralax Effect

---
+++ js/ParalaxWeb.js
@@ -0,0 +1,47 @@
+function ClickHandler()
+{
+    $("nav a").click(function(e)
+     {
+        e.preventDefault();
+        var slideId = $(this).attr('href');
+        console.log(slideId);
+        $("html body").animate({
+            scrollTop: $(slideId).offset().top,
+            duration: "slow",
+            easein: "easein"
+        },1000);
+    });
+}
+function CheckIfInView()
+{
+
+  var animation_elements = $('.subslide');
+  var Swindow = $(window);
+  var window_height = $(Swindow).height();
+  var window_top_position = $(Swindow).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($(animation_elements), function()
+  {
+    console.clear();
+    var element = $(this);
+    var element_height = element.height();
+    var element_top_position = element.offset().top;
+    var currentOffsetTop = Math.abs(window_height - element_top_position);
+    var element_bottom_position = (element_top_position + element_height);
+    console.log("element_top_position: "+element_top_position);
+    console.log("element_bottom_position: "+element_bottom_position);
+    console.log("window_top_position: "+window_top_position);
+    console.log("currentOffsetTop: "+currentOffsetTop);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= currentOffsetTop) && (element_top_position <= currentOffsetTop))
+    {
+      element.addClass('in-view');
+    }
+    else
+    {
+      element.removeClass('in-view');
+    }
+  });
+}
