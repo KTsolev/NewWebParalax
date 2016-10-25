e83bed2dedc9f0cb5abf707d03dc8326ed98cef1
KTsolev <ktsolev@gmail.com>
2016-10-26 01:02:46 +0200

    Switched from less to sass
    Sass is more flexible

--- js/ParalaxWeb.js
+++ js/ParalaxWeb.js
@@ -31,89 +31,114 @@ function ClickHandler()
     });
 }
 
-function Animation(toAnimate)
+function Animation(toAnimate,animationClass)
 {
-      var jsonObj = [];
-      var animationElements = $(toAnimate).children('img');  
-      var parent = new Node();
-      var container = $(toAnimate).parent();
-      parent.name = "parent";
-      parent.id = $(toAnimate).attr('id');
-      parent.class = $(toAnimate).attr('class');  
-      parent.offsetTop = $(container).offset().top;
-      var currentOffset = window.pageYOffset;
-      parent.scroll = currentOffset;
-      if(currentOffset < parent.offsetTop)
-      {
-        parent.bottomPos = parent.offsetTop + $(container).height()/2;
-        currentOffset += parent.offsetTop/4;
-      }
-      if(currentOffset > parent.bottomPos)
-      {
-        parent.bottomPos = parent.offsetTop - $(container).height()/2;
-        currentOffset -= parent.offsetTop/4;
-      }
-      if(currentOffset >= parent.offsetTop && currentOffset <= parent.bottomPos)
-      {
-          parent.isInView = true;
-          jsonObj.push({"parent" : parent});
-          $(toAnimate).addClass('expandY');
-      }
-      else
-      {
-          $(toAnimate).removeClass("expandY"); 
-      }  
-
-      var storage = WriteToLocalStorage(jsonObj);
-      var obj = ReadFromLocalStorage(storage);
-      ParseJSON(obj);
+  if(IsElementInView(toAnimate))
+  {
+      $(toAnimate).addClass(animationClass);
   }
+  else
+  {
+      $(toAnimate).removeClass(animationClass);
+  }
+}
+
+function IsElementInView(toAnimate)
+{
+  //var jsonObj = [];
+  var isInView = false;
+  var animationElements = $(toAnimate).children('img');  
+  var parent = new Node();
+  var container = $(toAnimate).parent();
+  parent.name = "parent";
+  parent.id = $(toAnimate).attr('id');
+  parent.class = $(toAnimate).attr('class');  
+  parent.offsetTop = $(container).offset().top;
+  var currentOffset = window.pageYOffset;
+  parent.scroll = currentOffset;
+  if(currentOffset < parent.offsetTop)
+  {
+    parent.bottomPos = parent.offsetTop + $(container).height()/2;
+    currentOffset += parent.offsetTop/4;
+  }
+  if(currentOffset > parent.bottomPos)
+  {
+    parent.bottomPos = parent.offsetTop - $(container).height()/2;
+    currentOffset -= parent.offsetTop/4;
+  }
+  if(currentOffset >= parent.offsetTop && currentOffset <= parent.bottomPos)
+  {
+      parent.isInView = true;
+   //   jsonObj.push({"parent" : parent});
+      isInView = true;
+  }
+ // var storage = WriteToLocalStorage(jsonObj);
+ // var obj = ReadFromLocalStorage(storage);
+ // ParseJSON(obj);
+  return isInView;
+}
   //expand objects, arrays console of chrome :D:D:D
   //$$('.console-view-object-properties-section').forEach(e => e._section._objectTreeElement.expandRecursively())
-
-//Animations//
-$.fn.animateRotate = function(angle, duration, easing, complete) {
-  var args = $.speed(duration, easing, complete);
-  var step = args.step;
-  return this.each(function(i, e) {
-    args.complete = $.proxy(args.complete, e);
-    args.step = function(now) {
-      $.style(e, 'transform', 'rotate(' + now + 'deg)');
-      if (step) return step.apply(e, arguments);
-    };
-
-    $({deg: 0}).animate({deg: angle}, args);
-  });
-};
-
-function Animation1(name,d)
+function GenerateHTML(container,tag)
 {
-    console.log(name);
-    var obj = $(name);
-    for (var i = obj.length - 1; i >= 0; i--) {
-      $(obj[i]).animateRotate(d,{
-        step: 15,
-        easin: 'easin',
-        duration:1000,
-        complete: function(){console.log("complete anim2");}
-  });
+    var parent = $(container);
+    var slides = [];
+    for (var j = 1; j <= 4; j++ )
+    {
+       var slide = document.createElement('div');
+       if(j % 2 == 0)
+       {
+          $(slide).attr('class','front_h'); 
+       }
+       else
+       {
+          $(slide).attr('class','back_h');
+       }
+       slides.push(slide);
     }
+    var l = 0;
+    for (var i = 1; i < 23; i++ ) 
+    {   
+        var child = document.createElement(tag);
+        $(child).attr('class','certs');
+        $(child).attr('src',"./images/certificates/"+ i +".jpg");
+        if($(slides[l]).children().length <= 3 )
+        {
+          $(slides[l]).append(child);
+        }
+        else
+        {
+          $(parent).append(slides[l]);
+          l++;
+        }  
+    }
+    console.log(slides);
 }
+
 //For Testing purposes//
 function WriteToLocalStorage(obj)
 {
-  if(typeof(localStorage)!== undefined)
+  if(typeof(localStorage) !== undefined)
+  {
    var myStorage = localStorage.setItem('myStorage', JSON.stringify(obj));
- else console.log("Browser doesnt support localStorage");
+  }
+  else
+  {
+   console.log("Browser doesnt support localStorage");
+  } 
    return myStorage;
 }
 
 function ReadFromLocalStorage(myStorage)
 {
   if(myStorage != undefined)
-  var obj = JSON.parse(myStorage.getItem('myStorage'));   
-else
-  var obj = JSON.parse(localStorage.getItem('myStorage'));
+  {  
+    var obj = JSON.parse(myStorage.getItem('myStorage'));   
+  }
+  else
+  {
+    var obj = JSON.parse(localStorage.getItem('myStorage'));
+  }
   return obj;
 }
 
@@ -152,11 +177,13 @@ function LoadCssFileDynamically(filename, filetype)
 {
     if (filetype=="css")
     { 
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
+        var fileref=document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
     }
     if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref)
+    {
+          document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
 }
